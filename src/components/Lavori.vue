8251d4654727c55c989d7e1f905f2915a453d555
Amedeo Pro <44196253+amedeopro@users.noreply.github.com>
2019-12-17 16:59:47 +0100

    implementato servizio recupero dati per edit
    implementato servizio recupero dati per edit

--- src/components/Lavori.vue
+++ src/components/Lavori.vue
@@ -1,234 +1,312 @@
 <template>
-<div>
+  <div>
     <el-row :gutter="10">
-        <h1>Lista Lavori</h1>
+      <h1>Lista Lavori</h1>
     </el-row>
     <el-row :gutter="10">
-        <el-button type="primary" @click="modalInserisciLavoro = true">Aggiungi Lavoro</el-button>
+      <el-button type="primary" @click="modalInserisciLavoro = true"
+        >Aggiungi Lavoro</el-button
+      >
     </el-row>
 
     <el-drawer
-            title="Aggiungi lavoro"
-            :visible.sync="modalInserisciLavoro"
-            direction="rtl"
-            custom-class="demo-drawer"
-            ref="drawer"
-            append-to-body
-            size="30%"
+      title="Aggiungi lavoro"
+      :visible.sync="modalInserisciLavoro"
+      direction="rtl"
+      custom-class="demo-drawer"
+      ref="drawer"
+      append-to-body
+      size="30%"
     >
-    <div style="padding: 0 30px 0 30px">
-        <el-form  label-width="160px" method="POST">
-            <el-form-item label="Cliente">
-                <el-select v-model="clienteScelto" placeholder="Cliente per cui svolgere il lavoro" >
-                    <el-option v-for="cliente in clienti" :key="cliente.id"
-                               :label="cliente.company"
-                               :value="cliente.id">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="Tipo di lavoro">
-                <el-input v-model="work_type"></el-input>
-            </el-form-item>
-            <el-form-item label="Consegnare entro">
-                <el-date-picker v-model="dead_line" type="date" format="dd-MM-yyyy" value-format="yyyy-MM-dd" placeholder="Seleziona una data"></el-date-picker>
-            </el-form-item>
-            <el-form-item label="Il lavoro è stato terminato">
-                <el-select v-model="finished" placeholder="Si o No?">
-                    <el-option
-                            label="Si"
-                            value="1">
-                    </el-option>
-                    <el-option
-                            label="No"
-                            value="0">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="A chi assegni il lavoro?">
-                <el-select v-model="utenteScelto" placeholder="Utente a cui assegnare il lavoro" >
-                    <el-option v-for="user in users" :key="user.id"
-                               :label="user.name"
-                               :value="user.id">
-                    </el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="Informazioni">
-                <el-input v-model="information" type="textarea" :rows="2"></el-input>
-            </el-form-item>
+      <div style="padding: 0 30px 0 30px">
+        <el-form label-width="160px" method="POST">
+          <el-form-item label="Cliente">
+            <el-select
+              v-model="clienteScelto"
+              placeholder="Cliente per cui svolgere il lavoro"
+            >
+              <el-option
+                v-for="cliente in clienti"
+                :key="cliente.id"
+                :label="cliente.company"
+                :value="cliente.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Tipo di lavoro">
+            <el-input v-model="work_type"></el-input>
+          </el-form-item>
+          <el-form-item label="Consegnare entro">
+            <el-date-picker
+              v-model="dead_line"
+              type="date"
+              format="dd-MM-yyyy"
+              value-format="yyyy-MM-dd"
+              placeholder="Seleziona una data"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="Il lavoro è stato terminato">
+            <el-select v-model="finished" placeholder="Si o No?">
+              <el-option label="Si" value="1"> </el-option>
+              <el-option label="No" value="0"> </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="A chi assegni il lavoro?">
+            <el-select
+              v-model="utenteScelto"
+              placeholder="Utente a cui assegnare il lavoro"
+            >
+              <el-option
+                v-for="user in users"
+                :key="user.id"
+                :label="user.name"
+                :value="user.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Informazioni">
+            <el-input
+              v-model="information"
+              type="textarea"
+              :rows="2"
+            ></el-input>
+          </el-form-item>
         </el-form>
 
         <el-button @click="modalInserisciLavoro = false">Annulla</el-button>
         <el-button type="primary" @click="inserisciLavoro()">Salva</el-button>
-    </div>
+      </div>
+    </el-drawer>
 
+    <el-drawer
+      title="Modifica lavoro"
+      :visible.sync="modalModificaLavoro"
+      direction="rtl"
+      custom-class="demo-drawer"
+      ref="drawer"
+      append-to-body
+      size="30%"
+      v-for="work in modWork"
+      :key="work.id"
+    >
+      <div style="padding: 0 30px 0 30px">
+        <el-form label-width="160px" method="POST">
+          <el-row> il cliente è {{ work.company }} </el-row>
+          <el-form-item label="Tipo di lavoro">
+            <el-input v-model="work.work_type"></el-input>
+          </el-form-item>
+          <el-form-item label="Consegnare entro">
+            <el-date-picker
+              v-model="work.dead_line"
+              type="date"
+              format="dd-MM-yyyy"
+              value-format="yyyy-MM-dd"
+              placeholder="Seleziona una data"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="Il lavoro è stato terminato">
+            <el-select v-model="work.finished" placeholder="Si o No?">
+              <el-option label="Si" value="1"> </el-option>
+              <el-option label="No" value="0"> </el-option>
+            </el-select>
+          </el-form-item>
+          <el-row> Attualemnte è assegnato a {{ work.name }} </el-row>
+          <el-form-item label="A chi assegni il lavoro?">
+            <el-select
+              v-model="utenteScelto"
+              placeholder="Utente a cui assegnare il lavoro"
+            >
+              <el-option
+                v-for="user in users"
+                :key="user.id"
+                :label="user.name"
+                :value="user.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Informazioni">
+            <el-input
+              v-model="work.information"
+              type="textarea"
+              :rows="2"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+
+        <el-button @click="modalModificaLavoro = false">Annulla</el-button>
+        <el-button type="primary" @click="modalModificaLavoro = false"
+          >Salva</el-button
+        >
+      </div>
     </el-drawer>
 
     <el-row :gutter="10">
-        <el-table
-                :data="lavori.filter(data => !search || data.company.toLowerCase().includes(search.toLowerCase()))"
-                style="width: 100%">
-            <el-table-column
-                    prop="name"
-                    label="Lavoro assegnato a"
-                    sortable
+      <el-table
+        :data="
+          lavori.filter(
+            data =>
+              !search ||
+              data.company.toLowerCase().includes(search.toLowerCase())
+          )
+        "
+        style="width: 100%"
+      >
+        <el-table-column prop="name" label="Lavoro assegnato a" sortable>
+        </el-table-column>
+        <el-table-column prop="company" label="Cliente" sortable>
+        </el-table-column>
+        <el-table-column prop="work_type" label="Tipo di Lavoro" sortable>
+        </el-table-column>
+        <el-table-column prop="dead_line" label="Terminare entro il" sortable>
+        </el-table-column>
+        <el-table-column prop="finished" label="E' stato terminato ?" sortable>
+        </el-table-column>
+        <el-table-column prop="information" label="Informazioni">
+        </el-table-column>
+        <el-table-column align="right">
+          <template slot-scope="scope">
+            <el-button size="mini" @click="modifyWork(scope.row.id)"
+              >Edit</el-button
             >
-            </el-table-column>
-            <el-table-column
-                    prop="company"
-                    label="Cliente"
-                    sortable
+            <el-button
+              size="mini"
+              type="danger"
+              @click="deleteWork(scope.row.id)"
+              >Delete</el-button
             >
-            </el-table-column>
-            <el-table-column
-                    prop="work_type"
-                    label="Tipo di Lavoro"
-                    sortable
-            >
-            </el-table-column>
-            <el-table-column
-                    prop="dead_line"
-                    label="Terminare entro il"
-                    sortable
-            >
-            </el-table-column>
-            <el-table-column
-                    prop="finished"
-                    label="E' stato terminato ?"
-                    sortable
-            >
-            </el-table-column>
-            <el-table-column
-                    prop="information"
-                    label="Informazioni"
-            >
-            </el-table-column>
-            <el-table-column
-                    align="right">
-                <template slot-scope="scope">
-                    <el-button
-                            size="mini"
-                            @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-                    <el-button
-                            size="mini"
-                            type="danger"
-                            @click="deleteWork(scope.row.id)">Delete</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-row>
-</div>
+  </div>
 </template>
 
 <script>
-
-  import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Lavori',
-  data(){
+  name: "Lavori",
+  data() {
     return {
-        search: '',
-        lavori: [],
-        work_type: null,
-        dead_line: null,
-        finished: null,
-        information: null,
-        modalInserisciLavoro: false,
-        clienteScelto: null,
-        clienti: [],
-        users: [],
-        utenteScelto: null,
-        modificaLavoro: {
-
-        }
-    }
+      search: "",
+      lavori: [],
+      work_type: null,
+      dead_line: null,
+      finished: null,
+      information: null,
+      modalInserisciLavoro: false,
+      modalModificaLavoro: false,
+      clienteScelto: null,
+      clienti: [],
+      users: [],
+      utenteScelto: null,
+      modWork: []
+    };
   },
-  created(){
-    this.works()
-    this.caricaUsers()
-    this.customers()
+  created() {
+    this.works();
+    this.caricaUsers();
+    this.customers();
   },
-  methods:{
-      handleEdit(){
-          // eslint-disable-next-line no-undef
-        Console.log('modifica')
-      },
-      handleDelete: function () {
-          // eslint-disable-next-line no-undef
-          Console.log('chiudi')
-      },
-    works(){
-      axios
-      .get('http://80.211.134.4/api/works')
-      .then(response => {
-        this.lavori = response.data;
-      })
-      .catch(error => {
-         return error
-      })
+  methods: {
+    handleEdit() {
+      // eslint-disable-next-line no-undef
+      Console.log("modifica");
     },
-      inserisciLavoro() {
-          axios
-              .post('http://80.211.134.4/api/works' + '?work_type=' + this.work_type + '&dead_line=' + this.dead_line + '&finished=' + this.finished + '&information=' + this.information + '&user_id=' + this.utenteScelto + '&customer_id=' + this.clienteScelto, {})
-              .then(response => {
-                  this.modalInserisciLavoro = false
-                  this.works()
-                  return response
-
-              })
-              .catch(error => {
-                  this.modalInserisciLavoro = false
-                  return error
-              })
-      },
-      caricaUsers(){
-          axios
-              .get('http://80.211.134.4/api/users')
-              .then(response => {
-                  this.users = response.data;
-              })
-              .catch(error => {
-                  return error
-              })
-      },
-      customers(){
-          axios
-              .get('http://80.211.134.4/api/customers')
-              .then(response => {
-                  this.clienti = response.data;
-              })
-              .catch(error => {
-                  return error
-              })
-      },
-      deleteWork(id){
-          axios
-              .delete('http://80.211.134.4/api/works/' + id )
-              .then(response => {
-                  this.works()
-                  return response
-              })
-              .catch(error => {
-                  return error
-              })
-      },
-      modifyWork(id){
-          axios
-              .get('http://80.211.134.4/api/works/' + id )
-              .then(response => {
-                  this.works()
-                  return response
-              })
-              .catch(error => {
-                  return error
-              })
-      }
+    handleDelete: function() {
+      // eslint-disable-next-line no-undef
+      Console.log("chiudi");
+    },
+    works() {
+      axios
+        .get("http://80.211.134.4/api/works")
+        .then(response => {
+          this.lavori = response.data;
+        })
+        .catch(error => {
+          return error;
+        });
+    },
+    inserisciLavoro() {
+      axios
+        .post(
+          "http://80.211.134.4/api/works" +
+            "?work_type=" +
+            this.work_type +
+            "&dead_line=" +
+            this.dead_line +
+            "&finished=" +
+            this.finished +
+            "&information=" +
+            this.information +
+            "&user_id=" +
+            this.utenteScelto +
+            "&customer_id=" +
+            this.clienteScelto,
+          {}
+        )
+        .then(response => {
+          this.modalInserisciLavoro = false;
+          this.works();
+          return response;
+        })
+        .catch(error => {
+          this.modalInserisciLavoro = false;
+          return error;
+        });
+    },
+    caricaUsers() {
+      axios
+        .get("http://80.211.134.4/api/users")
+        .then(response => {
+          this.users = response.data;
+        })
+        .catch(error => {
+          return error;
+        });
+    },
+    customers() {
+      axios
+        .get("http://80.211.134.4/api/customers")
+        .then(response => {
+          this.clienti = response.data;
+        })
+        .catch(error => {
+          return error;
+        });
+    },
+    deleteWork(id) {
+      axios
+        .delete("http://80.211.134.4/api/works/" + id)
+        .then(response => {
+          this.works();
+          return response;
+        })
+        .catch(error => {
+          return error;
+        });
+    },
+    modifyWork(id) {
+      axios
+        .get("http://80.211.134.4/api/works/" + id)
+        .then(response => {
+          this.modWork = response.data;
+          this.modalModificaLavoro = true;
+          return response;
+        })
+        .catch(error => {
+          return error;
+        });
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-    .el-row {
-        margin-bottom: 20px;
-    }
+.el-row {
+  margin-bottom: 20px;
+}
 </style>
