de9928148e0b301727a3eae68f2cb16189f1cc8e
Amedeo Pro <44196253+amedeopro@users.noreply.github.com>
2019-12-27 12:47:34 +0100

    route login
    route login

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@
 import Lavori from "./components/Lavori";
 import Dashboard from "./components/Dashboard";
 import ListaClienti from "./components/ListaClienti";
-
+import Login from "./components/Login";
 //Vue.use(VueRouter)
 
 export const routes = [
         {
             path: '/',
             component: Dashboard
+        },
+        {
+            path: '/login',
+            component: Login
         },
         {
             path: '/lavori',
