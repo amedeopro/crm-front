85a566ba2e1f2b8e5480086fe745bcc8bc2c223b
Amedeo Pro <44196253+amedeopro@users.noreply.github.com>
2019-12-30 10:27:01 +0100

    fix auth.js
    fix auth.js

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,18 +7,40 @@ export default {
         user: ''
     },
     mutations:{
-        //
+        SET_TOKEN(state, token){
+            state.token = token
+        },
+        SET_USER(state, data){
+            state.user = data
+        }
     },
     actions:{
-        async login(_, credentials) {
-            axios
+        async login({ dispatch }, credentials) {
+            await axios
                 .post("auth/signin", credentials)
                 .then(response => {
+                    dispatch('attempt', response.data.token)
                     return response;
                 })
                 .catch(error => {
                     return error;
                 });
+        },
+
+        async attempt({commit}, token){
+            commit('SET_TOKEN', token)
+
+            try{
+                let response = await axios.get('auth/me',{
+                    headers:{
+                        'Authorization': 'Bearer ' + token
+                    }
+                })
+                commit('SET_USER', response.data)
+            } catch(e){
+                commit('SET_TOKEN', null)
+                commit('SET_USER', null)
+            }
         }
     }
 }
