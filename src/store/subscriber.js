ec01f3f80da60f9dc61d9ddacb7c1fef98b613c5
Amedeo Pro <44196253+amedeopro@users.noreply.github.com>
2019-12-31 09:09:13 +0100

    fix mutation.payload
    fix mutation.payload

--- src/store/subscriber.js
+++ src/store/subscriber.js
@@ -5,7 +5,7 @@ store.subscribe((mutation) =>{
     switch(mutation.type){
         case 'auth/SET_TOKEN':
             if(mutation.payload){
-                axios.defaults.headers.common['Authorization'] = 'Bearer $(mutation.payload)'
+                axios.defaults.headers.common['Authorization'] = "Bearer" + mutation.payload
                 localStorage.setItem('token', mutation.payload)
             } else {
                 axios.defaults.headers.common['Authorization'] = null
